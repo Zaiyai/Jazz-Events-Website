05d5c7c245059e8452d037e2078f2896a3e22cab
Cyclobl <edrian.albero0@gmail.com>
2025-12-01 23:52:18 +0800

    fixed js for automating images for logo and decor

--- scripts/main.js
+++ scripts/main.js
@@ -20,10 +20,17 @@ document.querySelectorAll('.read-more-btn').forEach(btn => {
     });
 });
 
-document.querySelectorAll(".logo").forEach(logo => {
-   logo.src = "assets/Jazz.svg"; 
-});
+const fullPath = window.location.pathname;
+const lastSlashIndex = fullPath.lastIndexOf('/');
+const fileName = fullPath.substring(lastSlashIndex + 1);
 
-document.querySelectorAll(".gold_accent").forEach(accent => {
-    accent.src = "assets/decor/gold_accent.png";
-});
+var logoPath = "assets/Jazz.svg";
+var decorPath = "assets/decor/gold_accent.png";
+
+if (fileName != "home.html") {
+    logoPath = "../" + logoPath;
+    decorPath = "../" + decorPath;
+}
+
+document.querySelectorAll(".logo").forEach(logo => { logo.src = logoPath; });
+document.querySelectorAll(".gold_accent").forEach(accent => { accent.src = decorPath; });
