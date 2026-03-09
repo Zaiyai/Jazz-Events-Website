7f9c61b155891d8f048699b2ab92c167baf3d96c
bjorntosaurus <kbjornmmaldo@gmail.com>
2026-03-09 21:04:16 +0800

    added initial calendar feature, together with its  js and css

---
+++ booking/booking.js
@@ -0,0 +1,52 @@
+document.addEventListener('DOMContentLoaded', function () {
+
+  const calendarEl = document.getElementById('calendar');
+
+  const calendar = new FullCalendar.Calendar(calendarEl, {
+
+    initialView: 'dayGridMonth',
+
+    selectable: true,
+
+    dateClick: function(info) {
+      alert("You selected: " + info.dateStr);
+    }
+
+  });
+
+  calendar.render();
+
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+
+  const calendarEl = document.getElementById('calendar');
+
+  const calendar = new FullCalendar.Calendar(calendarEl, {
+
+    initialView: 'dayGridMonth',
+
+    events: [
+      {
+        title: "Booked",
+        start: "2026-03-10",
+        color: "red"
+      },
+      {
+        title: "Booked",
+        start: "2026-03-15",
+        color: "red"
+      }
+    ],
+
+    dateClick: function(info) {
+      alert("You selected: " + info.dateStr);
+    }
+
+  });
+
+  calendar.render();
+
+});
+
+
