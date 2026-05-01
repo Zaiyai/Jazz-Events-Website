747a1d74367c7d3bd8cafb7818572e1628d47084
Cyclobl <edrian.albero0@gmail.com>
2026-05-01 23:26:16 +0800

    added limitations to dates, and no. of attendees

--- booking/booking.js
+++ booking/booking.js
@@ -1,6 +1,19 @@
+const today = new Date().toISOString().split('T')[0];
+document.getElementById('datefrom').setAttribute('min', today);
+document.getElementById('dateto').setAttribute('min', today);
+
+const eventBudget = document.getElementById('event-budget');
+
 const attendees = document.getElementById('attendees');
 
 function updateEstimatedBudget() {
-    let servicesChecked = document.querySelectorAll('input[type="checkbox"]:checked').length;
-    document.getElementById('event-budget').innerHTML = 1000 * servicesChecked * attendees.value;
+    if (attendees.value > 99999) {
+        eventBudget.innerHTML = "Too many guests";
+    } else if (attendees.value < 1) {
+        eventBudget.innerHTML = "Too little guests";
+    } else {
+        let servicesChecked = document.querySelectorAll('input[type="checkbox"]:checked').length;
+        let totalBudget = 1000 * servicesChecked * attendees.value
+        eventBudget.innerHTML = totalBudget.toLocaleString();
+    }
 }
